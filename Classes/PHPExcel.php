066f93a44ee9aea4b322b416cb75021bd8dfc517
Solaxy <70860748+Solaxy@users.noreply.github.com>
2021-08-01 16:54:20 -0500

    Update PHPExcel.php

--- Classes/PHPExcel.php
+++ Classes/PHPExcel.php
@@ -11,7 +11,7 @@ if (!defined('PHPEXCEL_ROOT')) {
  *
  * Copyright (c) 2006 - 2015 PHPExcel
  *
- * This library is free software; you can redistribute it and/or
+ * This library is a free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
